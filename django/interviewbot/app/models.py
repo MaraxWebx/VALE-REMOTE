09f4fef279b623de3aee6269b5fd280c472687cb
PeppeT <nestadj123@gmail.com>
2020-10-26 17:51:33 +0100

    spostata definizione dell'interview dopo quella dell'user

--- django/interviewbot/app/models.py
+++ django/interviewbot/app/models.py
@@ -3,9 +3,6 @@ from django import forms
 import datetime
 
 # Create your models here.
-class Interview(models.Model):
-	date = models.DateTimeField('date published', auto_now_add=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Question(models.Model):
 	type = models.CharField(max_length=15, default="", null=True, blank=True)
@@ -17,6 +14,10 @@ class User(models.Model):
 	firstname = models.CharField(max_length=50)
 	lastname = models.CharField(max_length=50)
 	email = models.CharField(max_length=100)
+
+class Interview(models.Model):
+	date = models.DateTimeField('date published', auto_now_add=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Answer(models.Model):
 	interview = models.ForeignKey(Interview, on_delete=models.CASCADE)
