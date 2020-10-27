42263301c75ee025ec2adddcb07ce3c2d52970ba
PeppeT <nestadj123@gmail.com>
2020-10-27 15:07:16 +0100

    aggiunto QuestionFlow

--- django/interviewbot/app/admin.py
+++ django/interviewbot/app/admin.py
@@ -3,6 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register(QuestionFlow)
 admin.site.register(User)
 admin.site.register(VideoModel)
 admin.site.register(Interview)
