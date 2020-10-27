1f67111c3ac2c01a7f2e746b73489226d647df2d
PeppeT <nestadj123@gmail.com>
2020-10-27 12:32:21 +0100

    typofix

--- django/interviewbot/app/next_question.py
+++ django/interviewbot/app/next_question.py
@@ -4,24 +4,24 @@ class QuestionServer():
     dictlist_questions = []
     is_init = False
 
-    def __init__():
+    def __init__(self):
         qs_question = Question.objects.all().order_by('id')
 
         for question in qs_question:
-            dictlist_questions.append({
-                'type'      :   question.type
-                'action'    :   question.action
-                'length'    :   question.length
+            self.dictlist_questions.append({
+                'type'      :   question.type,
+                'action'    :   question.action,
+                'length'    :   question.length,
                 'choices'   :   question.choices
             })
         
-        is_init = True
+        self.is_init = True
 
-    def get(question_id):
-        if not is_init:
+    def get(self, question_id):
+        if not self.is_init:
             return 'Not initialized'
-        if question_id >=0 and question_id < len(dictlist_questions):
-            return dictlist_questions[question_id]
+        if question_id >=0 and question_id < len(self.dictlist_questions):
+            return self.dictlist_questions[question_id]
         return 'Invalid index input'
 
 def get_next_question():
