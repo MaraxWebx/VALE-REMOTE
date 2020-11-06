f1686fd75a58fd6158d63bc3f6db3ad3e996e8bb
PeppeT <nestadj123@gmail.com>
2020-11-06 10:17:18 +0100

    test upload file

--- site/public/static/js/credentials.js
+++ site/public/static/js/credentials.js
@@ -1,45 +1,80 @@
-function emailIsValid(email = document.getElementById("email").value) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
 
 
 function saveData() {
-  var firstName = document.getElementById("firstname");
-  var lastName = document.getElementById("lastname");
-  var email = document.getElementById("email");
-
-
-
-
-  if (emailIsValid(email.value)) {
-    alert(`
-               Nome: ${firstName.value} 
-               Cognome: ${lastName.value} 
-               Email: ${email.value}`);
-
-
-    (async () => {
-
-      axios.post('/restex/', {
-        //rejectUnauthorized: false,
-        firstname: firstName.value,
-        lastname: lastName.value,
-        email: email.value
-      })
-        .then((response) => {
-          console.log(response);
-        }, (error) => {
-          console.log(error);
-        });
-
-
-      const response = await axios({
-        url: '/restex/',
-        method: 'get'
-      })
-      console.log(response);
-
-    })()
+   var firstName = document.msform.firstname.value;
+   var lastName = document.msform.lastname.value;
+   var eMail = document.msform.email.value;
+   
+   
+ 
+ console.log("questo -------------", this.file);
+ 
+   if ((firstName == "") || (firstName == "undefined")) {
+     swal("Inserire Nome!","", "error");
+     document.msform.firstname.focus();
+     return false;
+ }
+ if ((lastName == "") || (lastName == "undefined")) {
+     swal("Inserire Cognome!","","error");
+     document.msform.lastname.focus();
+     return false;
+ }
+ var email_valid = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/;
+ if (!email_valid.test(eMail) || (eMail == "") || (eMail == "undefined")) {
+     swal("Inserire un indirizzo Email corretto!","","error");
+     document.msform.email.focus();
+     return false;
+ }
+ if(this.file.type=="file"){
+   swal("allegare CV!","", "error");
+   document.getElementById("file").focus();
+   return false;
   }
-}
+ 
+  let formData = new FormData();
+   formData.append('file', this.file);
+   console.log('>> formData >> ', formData);
+   console.log(this.file.type);
+ 
+   {
 
+     swal("In bocca al lupo!","","success");
+     (async () => {
+ 
+       axios.post('/file', {
+ 
+         //rejectUnauthorized: false,
+        // firstname: firstName,
+        // lastname: lastName,
+         //email: eMail,
+         formData,
+           headers: {
+             'Content-Type': 'multipart/form-data'
+           }
+         
+       })
+         .then((response) => {
+           console.log(response);
+         }, (error) => {
+           console.log(error);
+         });
+ 
+ 
+       const response = await axios({
+         url: 'file/',
+         method: 'get'
+       })
+       console.log(response);
+ 
+     })()
+   }
+   document.msform.reset();
+   this.files = [];
+ }
+ 
+ handleFileUpload = function (){
+   this.file= document.getElementById("file").files[0];
+   console.log('>>>> 1st element in files array >>>> ', file);
+ 
+ }
+ 
