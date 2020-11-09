dbed4bd79a7f064f1cce9c20650c8fee2c80b656
PeppeT <nestadj123@gmail.com>
2020-11-09 17:55:31 +0100

    bugfix

--- site/public/static/js/credentials.js
+++ site/public/static/js/credentials.js
@@ -1,6 +1,90 @@
 var file;
 
 function saveData() {
+
+
+  var firstName = document.msform.firstname.value;
+  var lastName = document.msform.lastname.value;
+  var eMail = document.msform.email.value;
+
+
+  if (firstName === "") {
+    swal("Inserire Nome!", "", "error");
+    document.msform.firstname.focus();
+    return false;
+  }
+
+  if (lastName === "") {
+    swal("Inserire Cognome!", "", "error");
+    document.msform.lastname.focus();
+    return false;
+  }
+
+  var email_valid = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/;
+  if (!email_valid.test(eMail)) {
+    swal("Inserire un indirizzo Email corretto!", "", "error");
+    document.msform.email.focus();
+    return false;
+  }
+
+
+
+  if (this.file === undefined) {
+    swal("allegare CV!", "", "error");
+    document.getElementById("file").focus();
+    return false;
+  }
+
+  axios.post('/restex/', {
+    firstname: firstName,
+    lastname: lastName,
+    email: eMail,
+  }, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  ).then((response) => {
+    console.log('Response:', response);
+    let formData = new FormData();
+    formData.append('file', file);
+
+    axios.post('/file/',
+      formData, {
+      headers: {
+        'Content-Type': 'application/pdf'
+      }
+    }
+    ).then(function () {
+      swal("Inizio intervista", "", "success");
+      console.log('SUCCESS!!');
+    })
+      .catch(function () {
+        swal("errore", "", "error");
+        console.log('FAILURE!!');
+      });
+    
+  }, (error) => {
+    swal("errore connessione server", "Riprova", "error");
+    console.log(error);
+  });
+
+
+
+
+  document.msform.reset();
+  this.files = [];
+}
+
+handleFileUpload = function () {
+  file = document.getElementById("file").files[0];
+  console.log('>>>> 1st element in files array >>>> ', file);
+
+}
+/*
+var file;
+
+function saveData() {
    var firstName = document.msform.firstname.value;
    var lastName = document.msform.lastname.value;
    var eMail = document.msform.email.value;
@@ -25,14 +109,7 @@ function saveData() {
      document.msform.email.focus();
      return false;
  }
- /*
- if(this.file.type=="file"){
-   swal("allegare CV!","", "error");
-   document.getElementById("file").focus();
-   return false;
-  }
-  */
- 
+
     axios.post('/restex/', { 
       firstname: firstName,
       lastname: lastName,
@@ -76,4 +153,4 @@ function saveData() {
    console.log('>>>> 1st element in files array >>>> ', file);
  
  }
- 
+ */
