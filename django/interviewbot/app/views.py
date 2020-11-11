a8231ede1795144f68de091981300b9d86704b43
PeppeT <nestadj123@gmail.com>
2020-11-11 11:00:42 +0100

    bugfix

--- django/interviewbot/app/views.py
+++ django/interviewbot/app/views.py
@@ -18,9 +18,7 @@ from app.form import *
 from app.text_analyzer import TextAnalyzer
 from app.text_filter import Filter
 
-from keras import backend as K
-
-K.clear_session()
+from keras.models import load_model
 
 """
 bow_path = "/var/www/site/bow.json"
