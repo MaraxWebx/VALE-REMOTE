e8e56be71d6423a8760e5917f1e70585ddfe2fb1
PeppeT <nestadj123@gmail.com>
2020-11-11 16:49:33 +0100

    bugfix

--- django/interviewbot/app/views.py
+++ django/interviewbot/app/views.py
@@ -17,6 +17,9 @@ from app.models import *
 from app.form import *
 from app.text_analyzer import TextAnalyzer
 from app.text_filter import Filter
+from app.text_sentiment import SentimentAnalyzer
+
+SA = SentimentAnalyzer()
 
 
 """
@@ -163,7 +166,7 @@ class NextQuestionView(APIView):
 				filter = Filter()
 				filter_results = filter.execute(analyze_results)
 
-				sentiment = settings.SA.execute(filter_results)
+				sentiment = SA.execute(filter_results)
 
 				key_max = ""
 				value_max = -999999999
