b2729c1108f18d6fe467d43b36c9131475142eb3
PeppeT <nestadj123@gmail.com>
2020-11-12 12:18:44 +0100

    bugfix

--- django/interviewbot/app/views.py
+++ django/interviewbot/app/views.py
@@ -175,7 +175,7 @@ class NextQuestionView(APIView):
 				else:
 					return 0
 			else:
-				if int(session.get('last_base_quest',-1)) < 0  or not session.get('have_forked', False):
+				if not session.get('have_forked', False):
 					session['last_base_quest'] = id
 				session['have_forked'] = True
 				
