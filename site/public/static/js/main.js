9fb19b73b8289905704b7a7fbb2e99cd0ffdce07
PeppeT <nestadj123@gmail.com>
2020-11-20 13:22:29 +0100

    dashboard

--- site/public/static/js/main.js
+++ site/public/static/js/main.js
@@ -106,6 +106,15 @@ function startCamera() {
 	}
 }
 
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
 
 function onBtnRecordClicked() {
 	if (localStream == null) {
@@ -119,6 +128,7 @@ function onBtnRecordClicked() {
 		stopBtn.hidden = false;
 		//progress(60, 60, $('#progressBar'));		//set time to progress bar
 		//startTimer(10, document.querySelector('#time'));
+		
 
 		/* use the stream */
 		log('Start recording...');
@@ -172,9 +182,10 @@ function onBtnRecordClicked() {
 
 			stopBtn.hidden = true;
 			//downloadLink.innerHTML = '<button class="okButton" id="controls"><p>Scarica</p></button>';
-
-			var rand = Math.floor((Math.random() * 10000000));
-			var name = "video_" + rand + ".webm";
+			var d = new Date();
+			var date = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
+			var uuid = window.create_UUID();
+			var name = "video_" + uuid + "_" + date + ".webm";
 			let file = new File([blob], name, { type: "video/webm" });
 
 			//downloadLink.setAttribute("download", file.name);
