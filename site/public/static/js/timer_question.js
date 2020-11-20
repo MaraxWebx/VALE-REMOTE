c8306be26cceb231cf2b202db89fa2acca15c715
PeppeT <nestadj123@gmail.com>
2020-11-20 15:44:20 +0100

    bugfix

---
+++ site/public/static/js/timer_question.js
@@ -0,0 +1,38 @@
+
+var question_time = 120;
+var timer_id;
+
+function restart_timer(){
+    display = document.querySelector("#timer_time_left")
+    start_timer(display, question_time)
+}
+
+function countdown_finished(){
+    clearInterval(timer_id)
+    // Gestire fine del tempo!
+
+}
+
+function start_timer(display, duration){
+    var timer = duration, minutes, seconds;
+    timer_id = setInterval(function () {
+        if (timer < 0) {
+            display.textContent = "Tempo scaduto!";
+            countdown_finished()
+            timer = duration;
+            return;
+        }
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        timer--;
+
+        
+    }, 1000);
+
+}
