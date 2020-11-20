0097410389609c806393855d9469a26d2028902f
PeppeT <nestadj123@gmail.com>
2020-11-20 15:58:09 +0100

    bugfix

--- site/public/static/js/timer_question.js
+++ site/public/static/js/timer_question.js
@@ -15,6 +15,21 @@ function countdown_finished(){
 
 function start_timer(display, duration){
     var timer = duration, minutes, seconds;
+    if (timer < 0) {
+        display.textContent = "Tempo scaduto!";
+        countdown_finished()
+        timer = duration;
+        return;
+    }
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = "Tempo rimanente: " + minutes + ":" + seconds;
+
+    timer--;
     timer_id = setInterval(function () {
         if (timer < 0) {
             display.textContent = "Tempo scaduto!";
@@ -31,8 +46,6 @@ function start_timer(display, duration){
         display.textContent = "Tempo rimanente: " + minutes + ":" + seconds;
 
         timer--;
-
-        
     }, 1000);
 
 }
