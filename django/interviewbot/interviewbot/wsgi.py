feeb617d2893452c5cff6acf327d04ab72277207
PeppeT <nestadj123@gmail.com>
2020-11-25 17:11:19 +0100

    bugfix

--- django/interviewbot/interviewbot/wsgi.py
+++ django/interviewbot/interviewbot/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviewbot.settings')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
 from django.contrib.auth.handlers.modwsgi import check_password
 from django.core.handlers.wsgi import WSGIHandler
