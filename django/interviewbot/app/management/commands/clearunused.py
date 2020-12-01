e63987185752189f06f69c9b8762e36ae057c35e
PeppeT <nestadj123@gmail.com>
2020-12-01 13:01:55 +0100

    bugfix

--- django/interviewbot/app/management/commands/clearunused.py
+++ django/interviewbot/app/management/commands/clearunused.py
@@ -40,6 +40,7 @@ class Command(BaseCommand):
             for vid in vids:
                 if vid.endswith(filename):
                     flag = True
+                    self.stdout.write(self.style.SUCCESS('This vid will NOT be deleted: %s. Path: %s%s%s' % filename,dir,video_folder,filename))
                     break
             if not flag:
                 # os.remove(dir + video_folder + '/' + filename)
