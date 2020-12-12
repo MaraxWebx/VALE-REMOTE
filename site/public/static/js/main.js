2172728ee552451da008d616201699df4dc1311e
altair363 <nestadj123@gmail.com>
2020-12-12 13:24:05 +0100

    fix refresh

--- site/public/static/js/main.js
+++ site/public/static/js/main.js
@@ -207,6 +207,7 @@ function onBtnRecordClicked() {
 
 		mediaRecorder.onstop = function () {
 			log('mediaRecorder.onstop, mediaRecorder.state = ' + mediaRecorder.state);
+			console.log('caller: ' + arguments.callee.caller.toString())
 
 			var blob = new Blob(chunks, { type: "video/webm" });
 			chunks = [];
