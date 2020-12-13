7415ed425b1141eccd3e91f5912ad87033a5bb7c
PeppeT <nestadj123@gmail.com>
2020-12-13 16:51:14 +0100

    bugfix

--- site/public/static/js/credentials.js
+++ site/public/static/js/credentials.js
@@ -115,20 +115,17 @@ function terms() {
 }
 
 function getBrowser() {
-
-  var browserName = '';
-  var userAgent = navigator.userAgent;
-  (typeof InstallTrigger !== 'undefined') && (browserName = 'Firefox');
-  ( /* @cc_on!@*/ false || !!document.documentMode) && (browserName = 'IE');
-  (!!window.chrome && userAgent.match(/OPR/)) && (browserName = 'Opera');
-  (!!window.chrome && userAgent.match(/Edge/)) && (browserName = 'Edge');
-  (!!window.chrome && !userAgent.match(/(OPR|Edge)/)) && (browserName = 'Chrome');
-
-  /**
-   * Expected returns
-   * Firefox, Opera, Edge, Chrome
-   */
-  return browserName;
+  agent = navigator.userAgent
+  switch (true) {
+    case agent.indexOf("edge") > -1: return "edge";
+    case agent.indexOf("edg/") > -1: return "chromium based edge (dev or canary)"; // Match also / to avoid matching for the older Edge
+    case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
+    case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
+    case agent.indexOf("trident") > -1: return "ie";
+    case agent.indexOf("firefox") > -1: return "firefox";
+    case agent.indexOf("safari") > -1: return "safari";
+    default: return "other";
+}
 
   
   /*
