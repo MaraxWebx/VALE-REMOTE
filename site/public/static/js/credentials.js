a9b22a15c3f079f4163cb3f6c8f9d78f3bfe439c
altair363 <nestadj123@gmail.com>
2020-12-14 10:18:20 +0100

    aggiunta permessi

--- site/public/static/js/credentials.js
+++ site/public/static/js/credentials.js
@@ -120,8 +120,8 @@ function getBrowser() {
   switch (true) {
     case agent.indexOf("edge") > -1: return "Edge";
     case agent.indexOf("edg/") > -1: return "Chromium based edge"; // Match also / to avoid matching for the older Edge
-    case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
-    case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+    case agent.indexOf("opr") > -1 : return "Opera";
+    case agent.indexOf("chrome") > -1: return "Chrome";
     case agent.indexOf("trident") > -1: return "ie";
     case agent.indexOf("firefox") > -1: return "Firefox";
     case agent.indexOf("safari") > -1: return "Safari";
